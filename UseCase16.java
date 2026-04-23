4f1a5eca7ea6896d39904a318c1a311bb49ffe68
VANI SRIVASTAVA <vs4886@srmist.edu.in>
2026-04-24 01:40:14 +0530

    Added UC16 Bubble Sort implementation

---
+++ UseCase16.java
@@ -0,0 +1,39 @@
+public class UseCase16 {
+
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" UC16 - Manual Sorting using Bubble Sort ");
+        System.out.println("========================================\n");
+
+        // Create array of passenger bogie capacities
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        // Display original array
+        System.out.println("Original Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        // ----- BUBBLE SORT -----
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+                    // swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        // Display sorted array
+        System.out.println("\n\nSorted Capacities (Ascending):");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        System.out.println("\n\nUC16 sorting completed...");
+    }
+}
